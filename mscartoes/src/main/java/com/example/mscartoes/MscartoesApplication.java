c932cf2960865da9049ff68cfd0de62db90ebbd2
tamireshc <tamireshc@gmail.com>
2023-12-05 11:57:20 -0300

    feat: init queues with rabbitmq

--- mscartoes/src/main/java/com/example/mscartoes/MscartoesApplication.java
+++ mscartoes/src/main/java/com/example/mscartoes/MscartoesApplication.java
@@ -1,13 +1,14 @@
 package com.example.mscartoes;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class MscartoesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscartoesApplication.class, args);
 	}
-
 }
